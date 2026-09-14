e627b734f2ec2fc1830785d22e9ae9b548558538
Apollo <zhanglay2004@gmail.com>
2026-09-14 21:37:35 +0800

    测试类创建

---
+++ maven-project01/src/test/java/com/itheima/UserServiceTest.java
@@ -0,0 +1,10 @@
+package com.itheima;
+
+import org.junit.jupiter.api.Test;
+
+public class UserServiceTest {
+    @Test
+    public void testAddUser() {
+        System.out.println("testAddUser");
+    }
+}
