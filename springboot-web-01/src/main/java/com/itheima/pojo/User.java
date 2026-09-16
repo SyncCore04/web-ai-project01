6670a2da95a131c177018389748843cf694aa956
Apollo <zhanglay2004@gmail.com>
2026-09-16 23:55:54 +0800

    UserController已完成

---
+++ springboot-web-01/src/main/java/com/itheima/pojo/User.java
@@ -0,0 +1,23 @@
+package com.itheima.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    // 封装用户信息
+    private Integer id;
+    private String username;
+    private String password;
+    private String name;
+    private Integer age;
+    private LocalDateTime updateTime;
+
+
+}
