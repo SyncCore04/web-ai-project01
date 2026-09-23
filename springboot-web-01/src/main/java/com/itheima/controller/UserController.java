445c619058c5bd24214196998543c1cb16c056d2
Apollo <zhanglay2004@gmail.com>
2026-09-23 22:40:44 +0800

    分层解耦，三层架构

--- springboot-web-01/src/main/java/com/itheima/controller/UserController.java
+++ springboot-web-01/src/main/java/com/itheima/controller/UserController.java
@@ -1,49 +1,21 @@
 package com.itheima.controller;
 
-import cn.hutool.core.io.IoUtil;
 import com.itheima.pojo.User;
+import com.itheima.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
-import static java.lang.System.in;
-
-//  用户信息控制器
-@RestController //子注解@ResponseBody -> 将Controller返回的数据转换为json格式
-
+@RestController
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
     @RequestMapping("/list")
-    public List<User> list() throws FileNotFoundException {
-        //1.加载并读取user.txt文件
-
-        InputStream in = this.getClass().getClassLoader().getResourceAsStream("user.txt");
-        ArrayList<String> lines = IoUtil.readLines(in, StandardCharsets.UTF_8,new ArrayList<>());
-
-        //2.解析文件内容，将用户信息封装到User对象中
-        List<User> userList = lines.stream().map(line -> {
-            String[] parts = line.split(",");
-            Integer id = Integer.parseInt(parts[0]);
-            String username = parts[1];
-            String password = parts[2];
-            String name = parts[3];
-            Integer age = Integer.parseInt(parts[4]);
-            LocalDateTime updateTime =
-                    LocalDateTime.parse(parts[5], DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-
-            return new User(id, username, password, name, age, updateTime);
-        }).collect(Collectors.toList());
-
-        //3.返回数据(json格式)
-        return userList;
+    public List<User> list() {
+        return userService.findAll();
     }
 }
